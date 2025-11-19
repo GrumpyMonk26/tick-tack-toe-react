9662ad4df1154eb99a286336b4594d9efae768fe
Jason Evans <evans.jason@hotmail.co.uk>
2025-11-19 22:22:08 +0000

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectedSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">

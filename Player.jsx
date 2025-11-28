2ee78f0c91b4ad73cc44bf0143e27e6b3bb5f75f
Jason Evans <53913153+GrumpyMonk26@users.noreply.github.com>
2025-11-28 12:43:36 +0000

    Add files via upload
    lifting state

---
+++ Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((prev) => !prev);
+  }
+
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
